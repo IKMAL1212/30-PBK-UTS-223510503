d932ab3904beea9972b804199399c7f20b241249
IKMAL1212 <165632052+IKMAL1212@users.noreply.github.com>
2024-06-23 19:10:04 +0700

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import AlbumsView from '../views/AlbumsView.vue';
-import AlbumPhotosView from '../views/AlbumPhotosView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,18 +12,10 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
-    }
-    {
-      path: '/albums',
-      name: 'albums',
-      component: AlbumsView
-    },
-    {
-      path: '/albums/:albumId',
-      name: 'album-photos',
-      component: AlbumPhotosView,
-      props: true
     }
   ]
 })
