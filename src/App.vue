5efd12623c5a79efd53e2bfa86d12a4f2940eb93
IKMAL1212 <165632052+IKMAL1212@users.noreply.github.com>
2024-06-23 21:14:47 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,48 +3,22 @@
     <nav class="navbar">
       <ul class="nav-list">
         <li :class="{ active: selectedMenu === 'todos' }">
-          <router-link to="/todos" class="nav-link" @click.native="showTodos">Todos</router-link>
+          <router-link to="/todos" class="nav-link">Todos</router-link>
         </li>
         <li :class="{ active: selectedMenu === 'posts' }">
-          <router-link to="/posts" class="nav-link" @click.native="showPosts">Posts</router-link>
+          <router-link to="/posts" class="nav-link">Posts</router-link>
         </li>
         <li :class="{ active: selectedMenu === 'albums' }">
-          <router-link to="/albums" class="nav-link" @click.native="showAlbums">Albums</router-link>
+          <router-link to="/albums" class="nav-link">Albums</router-link>
         </li>
       </ul>
     </nav>
 
     <div class="content">
       <router-view v-if="!selectedMenu"></router-view>
-      <PostsComponent
-        v-else-if="selectedMenu === 'posts'"
-        :users="users"
-        :selectedUser="selectedUser"
-        :userPosts="userPosts"
-        @fetch-posts="fetchPosts"
-      />
-      <TodosComponent
-        v-else-if="selectedMenu === 'todos'"
-        :tasks="tasks"
-        :newTask="newTask"
-        :incompleteTasks="incompleteTasks"
-        :showIncomplete="showIncomplete"
-        @add-task="addTask"
-        @cancel-task="cancelTask"
-        @edit-task="editTask"
-        @update-task="updateTask"
-        @delete-task="deleteTask"
-      />
-      <AlbumsComponent
-        v-else-if="selectedMenu === 'albums'"
-        :albums="albums"
-        :newAlbum="newAlbum"
-        @add-album="addAlbum"
-        @edit-album="editAlbum"
-        @update-album="updateAlbum"
-        @cancel-album="cancelAlbum"
-        @delete-album="deleteAlbum"
-      />
+      <PostsComponent v-else-if="selectedMenu === 'posts'" :users="users" :selectedUser="selectedUser" :userPosts="userPosts" @fetch-posts="fetchPosts" />
+      <TodosComponent v-else-if="selectedMenu === 'todos'" :tasks="tasks" :newTask="newTask" :incompleteTasks="incompleteTasks" :showIncomplete="showIncomplete" />
+      <AlbumsComponent v-else-if="selectedMenu === 'albums'" :albums="albums" :newAlbum="newAlbum" @add-album="addAlbum" @edit-album="editAlbum" @update-album="updateAlbum" @cancel-album="cancelAlbum" @delete-album="deleteAlbum" />
     </div>
 
     <!-- Animasi teks Selamat Datang -->
@@ -59,10 +33,9 @@
 import { ref } from 'vue';
 import PostsComponent from './components/PostsComponent.vue';
 import TodosComponent from './components/TodosComponent.vue';
-import AlbumsComponent from './views/Albums.vue';
+import AlbumsComponent from './views/Albums.vue'; // Pastikan ini mengarah ke komponen yang benar
 
 const selectedMenu = ref('');
-
 const users = ref([]);
 const selectedUser = ref(null);
 const userPosts = ref([]);
@@ -70,33 +43,12 @@ const tasks = ref([]);
 const newTask = ref('');
 const incompleteTasks = ref([]);
 const showIncomplete = ref(true);
-
 const albums = ref([]);
 const newAlbum = ref('');
 
 const showAlbums = () => { selectedMenu.value = 'albums'; };
 const showPosts = () => { selectedMenu.value = 'posts'; };
 const showTodos = () => { selectedMenu.value = 'todos'; };
-
-const addTask = () => {
-  // Definisi logika untuk addTask
-};
-
-const cancelTask = () => {
-  // Definisi logika untuk cancelTask
-};
-
-const editTask = () => {
-  // Definisi logika untuk editTask
-};
-
-const updateTask = () => {
-  // Definisi logika untuk updateTask
-};
-
-const deleteTask = () => {
-  // Definisi logika untuk deleteTask
-};
 
 const addAlbum = () => {
   if (newAlbum.value.trim() !== '') {
@@ -136,10 +88,10 @@ const fetchPosts = () => {
 
 <style scoped>
 .main-container {
-  background-image: url('../src/assets/gunung.png'); /* Path ke gambar background */
-  background-size: cover; /* Menutupi seluruh area background */
-  background-position: center; /* Posisi gambar di tengah-tengah */
-  min-height: 100vh; /* Tinggi minimum halaman */
+  background-image: url('../src/assets/gunung.png');
+  background-size: cover;
+  background-position: center;
+  min-height: 100vh;
   display: flex;
   flex-direction: column;
 }
@@ -151,7 +103,7 @@ const fetchPosts = () => {
   background-color: rgb(238, 180, 93);
   z-index: 1000;
   display: flex;
-  justify-content: center; /* Menengahkan navbar */
+  justify-content: center;
 }
 
 .nav-list {
@@ -172,37 +124,36 @@ const fetchPosts = () => {
   padding: 14px 20px;
   text-decoration: none;
   color: black;
-  position: relative; /* Menambahkan posisi relatif untuk animasi */
+  position: relative;
 }
 
 .nav-link:hover {
-  background-color: #e2e2e2; /* Warna latar saat hover */
+  background-color: #e2e2e2;
 }
 
 .active .nav-link {
-  background-color: #e6e6e6; /* Warna latar saat item aktif */
+  background-color: #e6e6e6;
 }
 
 .nav-link::before {
   content: '';
   position: absolute;
   bottom: 0;
-  left: 50%; /* Posisi awal tengah */
+  left: 50%;
   width: 0;
-  height: 3px; /* Tinggi garis animasi */
-  background-color: #000; /* Warna garis animasi */
-  transition: width 0.3s ease-out, left 0.3s ease-out; /* Transisi lebar dan posisi */
+  height: 3px;
+  background-color: #000;
+  transition: width 0.3s ease-out, left 0.3s ease-out;
 }
 
 .nav-link:hover::before {
-  width: 100%; /* Lebar garis saat hover */
-  left: 0; /* Posisi garis saat hover */
+  width: 100%;
+  left: 0;
 }
 
 .content {
   margin-top: 50px;
 }
-
 
 .welcome-text,
 .choose-text {
